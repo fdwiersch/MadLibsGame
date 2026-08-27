dbb031ece55d615ccc2e468eccaf2a304a5900d5
fwiersch <fionawiersch@gmail.com>
2026-08-27 13:31:18 -0700

    My madlib

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 int main()
 {
   // I. Variable declaration
-
-
+string exclame ;
+int wholenumber ;
+string adj ;
   // II. Prompt the user for input and get their responses
+cout << "enter an exclamation: ";
+cin >> exclame;
 
-  
+cout << "enter a whole number (>1): ";
+cin >> wholenumber;
+
+cout << "enter an adjective: ";
+cin >> adj;
   // III. Display the mad lib story
-
+cout << "'" << exclame << "!', I said on my way home when I suddenly saw " << wholenumber << " ladybugs. I had never seen such " << adj << " ones before." ;
   return 0;
 }
